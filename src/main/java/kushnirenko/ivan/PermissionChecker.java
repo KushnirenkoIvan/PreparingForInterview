727b82399c7c9c93978ff7f998d72e0f1a9d2831
Ivan <kushnirenkoivan@gmail.com>
2015-11-16 20:34:59 +0200

    Initial commit

---
+++ src/main/java/kushnirenko/ivan/PermissionChecker.java
@@ -0,0 +1,12 @@
+package kushnirenko.ivan;
+
+/**
+ * Created by ivan on 16/11/15.
+ */
+public class PermissionChecker {
+
+    public final byte ROLE_GUEST = 1;
+    public final byte ROLE_USER = 2;
+    public final byte ROLE_ADMIN = 3;
+
+}
